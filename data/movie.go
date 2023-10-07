73c56b853d5e02425f3b3b4264ba1bbf084d2998
Adam Elfsborg <adam.elfsborg@mtek.se>
2023-10-07 08:33:41 +0200

    feat: add movie logic

--- data/movie.go
+++ data/movie.go
@@ -2,15 +2,35 @@ package data
 
 import (
 	"github.com/adamelfsborg-code/movie-nest/config"
+	"github.com/adamelfsborg-code/movie-nest/db"
 	"github.com/adamelfsborg-code/movie-nest/pkg/themoviedb"
+	"github.com/google/uuid"
 )
 
 type MovieData struct {
 	Env config.Environments
 }
 
-func (u *MovieData) GetMovie(movieID uint) (*themoviedb.Movie, error) {
-	movieDB := themoviedb.NewMovieDBOptions(u.Env.MovieDBAuthToken, "")
+type Movie struct {
+	ID      uuid.UUID `json:"id" db:"id"`
+	MovieID uint      `json:"movie_id" db:"movie_id"`
+	ShelfID uuid.UUID `json:"shelf_id" db:"shelf_id"`
+}
+
+func NewMovie(movieID uint, shelfID uuid.UUID) *Movie {
+	return &Movie{
+		MovieID: movieID,
+		ShelfID: shelfID,
+	}
+}
+
+func (m *MovieData) CreateMovie(movie Movie) error {
+	_, err := db.Store.Model(&movie).Insert()
+	return err
+}
+
+func (m *MovieData) GetMovie(movieID uint) (*themoviedb.Movie, error) {
+	movieDB := themoviedb.NewMovieDBOptions(m.Env.MovieDBAuthToken, "")
 
 	movie, err := movieDB.GetMovie(movieID)
 	if err != nil {
--- handlers/movie.go
+++ handlers/movie.go
@@ -8,10 +8,35 @@ import (
 
 	"github.com/adamelfsborg-code/movie-nest/data"
 	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
 )
 
 type MovieHandler struct {
 	Data data.MovieData
+}
+
+func (m *MovieHandler) CreateMovie(w http.ResponseWriter, r *http.Request) {
+	var body struct {
+		MovieID uint      `json:"movie_id"`
+		ShelfID uuid.UUID `json:"shelf_id"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		fmt.Println("Failed to decode json: ", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	movie := data.NewMovie(body.MovieID, body.ShelfID)
+	err = m.Data.CreateMovie(*movie)
+	if err != nil {
+		fmt.Println("Failed to create movie: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
 
 func (m *MovieHandler) GetMovie(w http.ResponseWriter, r *http.Request) {
@@ -25,6 +50,11 @@ func (m *MovieHandler) GetMovie(w http.ResponseWriter, r *http.Request) {
 	}
 
 	movie, err := m.Data.GetMovie(uint(movieID))
+	if err != nil {
+		fmt.Println("Failed to get movie: ", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
 	jsonBytes, err := json.Marshal(movie)
 	if err != nil {
